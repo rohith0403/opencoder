497fb1b7daaa2cedd540197d934d07f6517868bf
sanjay037 <sanjaysingireddy01@gmail.com>
2021-11-09 16:14:13 +0530

    added css for marks

--- frontend/src/components/Marks.js
+++ frontend/src/components/Marks.js
@@ -1,64 +1,62 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getMarksCustom } from "../actions/marks";
+import { Link } from "react-router-dom";
+import { Badge } from 'react-bootstrap';
 let userDetails = JSON.parse(localStorage.getItem("user"));
 
 const MarksList = (props) => {
-  const [currentMarks, setcurrentMarks] = useState(null);
-  const [currentIndex, setCurrentIndex] = useState(-1);
-
+  const [totalmarks,setTotalMarks] = useState(0);
+  const [count,setCount] = useState(0);
   const marks = useSelector(state => state.marks);
+  const total_marks = () => {
+    var total = 0;
+    for(let i=0;i<marks.length;i++) total+=marks[i].marks;
+    setTotalMarks(total);
+    setCount(marks.length);
+  }
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getMarksCustom(userDetails.id,props.location.state.exam._id));
   }, [dispatch]);
 
-  const setActiveUser = (mark, index) => {
-    setcurrentMarks(mark);
-    setCurrentIndex(index);
-  };
-
+  useEffect(() => {
+    total_marks();
+  }, );
 
   return (
-    <div className="list row">
-      <div className="col-md-6">
-        <h4>Marks</h4>
-
-        <ul className="list-group">
-          {marks &&
-            marks.map((mark, index) => (
-              <li
-                className={
-                  "list-group-item " + (index === currentIndex ? "active" : "")
-                }
-                onClick={() => setActiveUser(mark, index)}
-                key={index}
-              >
-                {mark.questionname}
-              </li>
-            ))}
-        </ul>
-
-      </div>
-      <div className="col-md-6">
-        {currentMarks ? (
-          <div>
-            <h4>Marks</h4>
-            <div>
-              <label>
-                <strong>Marks:</strong>
-              </label>{" "}
-              {currentMarks.marks}
-            </div>
-          </div>
-        ) : (
-          <div>
-            <br />
-            <p>Please click on a User...</p>
-          </div>
-        )}
-      </div>
-    </div>
+    <div className="container">
+            <h3 className="p-3 text-center"></h3>
+            <table className="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th>Question</th>
+                        <th>Marks</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {marks && marks.map(mark =>
+                        <tr key={mark._id}>
+                            <td>{mark.questionname}</td>
+                            <td>{mark.marks}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+            <h2>Total marks  :  {totalmarks*10} out of {count*50}</h2>
+            <div className="home">
+            <Link
+              to={{
+                pathname: "/allexams",
+              }}
+              className="badge badge-warning"
+            >
+            <Badge bg="success" text="dark">
+              Home
+            </Badge>{' '}
+            </Link>
+      </div> 
+        </div>
   );
 };
 
